fabaa4781ca09fbd357735ef51da45204df47043
jiangkaili <jiangkaili@tuhu.cn>
2018-04-03 12:01:26 +0800

    更改目录

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # blog
+
+### 访问地址(https://Kally10307.github.io/blog/)
---
+++ src/views/frontend/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wp">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
